ac2935fb81ba824eb02c92fc6c078498c158b8c6
JuanPa7799 <garsa779999@gmail.com>
2023-08-25 09:04:44 -0600

    Se termino el modulo de diseño de la aplicacion, ahora seguira la aplicaciond e interaccion con react.js

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ import { CreateTodoButton } from "./CreateTodoButton";
 import React from "react";
 
 const defaultTodos = [
-  { text: `Cortar cebolla`, complete: false},
-  { text: `Tomar el curso de Introduccion a React.js`, complete: false},
-  { text: `Llorar con la llorona`, complete: false},
-  { text: `Going going going`, complete: false},
+  { text: `Cortar cebolla`, completed: true},
+  { text: `Tomar el curso de Introduccion a React.js`, completed: false},
+  { text: `Llorar con la llorona`, completed: false},
+  { text: `Going going going`, completed: false},
 ];
 
 function App() {
   return (
     <React.Fragment>
-      <TodoCounter complete={16} total={25}/>
+      <TodoCounter completed={16} total={25}/>
       <TodoSearch/>
 
       <TodoList>
         {defaultTodos.map(todo => (
-          <TodoItem key={todo.text} text={todo.text} complete={todo.complete}/>
+          <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
         ))}
       </TodoList>
 
