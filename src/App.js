c64eba5fe9a2670d099feb48a5579e5d2b86af38
JuanPa7799 <garsa779999@gmail.com>
2023-08-26 20:29:47 -0600

    Se termino el modulo sobre modificar iconos y mandar interaccion de los mismos (color check, delete).

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
   const completeTodo =(text)=>{
     const newTodos =[...todos];
     const todoIndex = newTodos.findIndex(
-      (todo) => todo.text == text
+      (todo) => todo.text === text
     );
     newTodos[todoIndex].completed = true;
     setTodos(newTodos);
@@ -41,7 +41,7 @@ function App() {
   const deleteTodo =(text)=>{
     const newTodos =[...todos];
     const todoIndex = newTodos.findIndex(
-      (todo) => todo.text == text
+      (todo) => todo.text ===text
     );
     newTodos.splice(todoIndex, 1);
     setTodos(newTodos);
